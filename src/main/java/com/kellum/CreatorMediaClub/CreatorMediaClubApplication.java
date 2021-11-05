2e06cc2bf67bd549096d8f19b539d1d5ad8dbc70
= <=Christopherk379@gmail.com>
2021-11-05 17:22:56 -0400

    init commit

---
+++ src/main/java/com/kellum/CreatorMediaClub/CreatorMediaClubApplication.java
@@ -0,0 +1,13 @@
+package com.kellum.CreatorMediaClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorMediaClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatorMediaClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kellum/CreatorMediaClub/CreatorMediaClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kellum.CreatorMediaClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorMediaClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
